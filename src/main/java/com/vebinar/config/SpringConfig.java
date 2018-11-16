86adcf78e3fb9a32831c0c3233ebdeae9ce3c9f2
jkill <jagernaut1233@gmail.com>
2018-11-16 15:42:32 +0200

    1

---
+++ src/main/java/com/vebinar/config/SpringConfig.java
@@ -0,0 +1,41 @@
+package com.vebinar.config;
+
+import com.vebinar.dao.UserDao;
+import com.vebinar.dao.UserDaoImpl;
+import com.vebinar.service.UserService;
+import com.vebinar.service.UserServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public JdbcTemplate getJdbcTemplate(){
+        return new JdbcTemplate(getDataSource());
+    }
+
+    @Bean
+    public DataSource getDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl("jdbc:mysql://localhost:3306/vebinar?useSll=false");
+        dataSource.setUsername("root");
+        dataSource.setPassword("jkk4250jkk");
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        return  dataSource;
+
+    }
+
+    @Bean
+    public UserDao getUserDao(){
+        return new UserDaoImpl(getJdbcTemplate());
+    }
+    @Bean
+    public UserService getUserService(){
+       return new UserServiceImpl();
+    }
+}
---
+++ src/main/java/com/vebinar/dao/UserDao.java
@@ -0,0 +1,17 @@
+package com.vebinar.dao;
+import com.vebinar.entity.User;
+
+import java.util.List;
+
+public interface UserDao {
+    void save(User user);
+
+    User getById(int id);
+
+    List<User> findAll();
+
+    void update(User user);
+
+    void delete(int id);
+
+}
